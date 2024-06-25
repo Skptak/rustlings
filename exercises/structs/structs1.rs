e52958b75ab38196ca1a47a7ac83924f3130d67a
Soren Ptak <skptak@amazon.com>
2024-06-25 14:56:34 -0600

    More progress on the rustlings

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -5,25 +5,32 @@
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+use std::fmt;
 
 struct ColorClassicStruct {
-    // TODO: Something goes here
+    red : u32,
+    green : u32,
+    blue : u32
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct(u32, u32, u32);
 
 #[derive(Debug)]
 struct UnitLikeStruct;
 
+
+impl std::fmt::Display for UnitLikeStruct {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f,"UnitLikeStruct")
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn classic_c_structs() {
-        // TODO: Instantiate a classic c struct!
-        // let green =
+        let green = ColorClassicStruct { red : 0, green : 255, blue : 0 };
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -32,8 +39,7 @@ mod tests {
 
     #[test]
     fn tuple_structs() {
-        // TODO: Instantiate a tuple struct!
-        // let green =
+        let green = ColorTupleStruct(0,255,0);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
@@ -43,7 +49,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit-like struct!
-        // let unit_like_struct =
+        let unit_like_struct = UnitLikeStruct;
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
